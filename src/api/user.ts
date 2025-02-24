1164de49753071ada71efe886448bdb5df91b8f5
Ali Sleiman <asleiman@acksession.com>
2025-02-25 00:46:45 +0200

    login

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,11 +1,11 @@
 import api from './api';
 
 // Define User DTO interfaces
-export const login = async (username: string, password: string): Promise<any> => {
+export const login = async (Username: string, password: string): Promise<any> => {
     const url = '/auth/login';
     try {
         // Use POST and send credentials in an object
-        const response = await api.post(url, { username, password });
+        const response = await api.post(url, { Username, password });
         return response.data;
     } catch (error) {
         console.error("Error logging in:", error);
